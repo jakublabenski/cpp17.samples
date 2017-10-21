99ff580e7788b70e53fd40124e3f70b5d2474fd9
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-21 19:07:19 +0200

    misc added.

---
+++ misc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  misc
+//
+//  Created by Jakub Łabeński on 21.10.2017.
+//  Copyright © 2017 Jakub Łabeński. All rights reserved.
+//
+
+// __has_include can be used to check if header file exists
+#if __has_include(<iostream>)
+#include <iostream>
+#endif
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
