c8ea36effc9110694c56f6f0115b774c9c4ef861
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-21 15:35:11 +0200

    Example for nested namespace definition added.

---
+++ nested namespace definition/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  nested namespace definition
+//
+//  Created by Jakub Łabeński on 21.10.2017.
+//  Copyright © 2017 Jakub Łabeński. All rights reserved.
+//
+
+namespace Outer::Inner {
+    
+}
+
+namespace Outer {
+    namespace Inner {
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
