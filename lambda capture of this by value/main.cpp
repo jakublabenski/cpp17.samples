194041634175e556c6b7c1d5da0b5ded0ad0b546
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-21 21:29:58 +0200

    lambda capture of this by value - example added

---
+++ lambda capture of this by value/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  lambda capture of this by value
+//
+//  Created by Jakub Łabeński on 21.10.2017.
+//  Copyright © 2017 Jakub Łabeński. All rights reserved.
+//
+
+#include <iostream>
+
+struct S {
+    void f() {
+        std::cout << "0: l = " << l << std::endl;
+        [this] () mutable { this->l = 1; }();
+        std::cout << "1: l = " << l << std::endl;
+        [*this] () mutable { this->l = 2; }();
+        std::cout << "2: l = " << l << std::endl;
+        [=, *this] () mutable { this->l = 3; }();
+        std::cout << "3: l = " << l << std::endl;
+    }
+    int l = 0;
+};
+
+int main(int argc, const char * argv[]) {
+    S s;
+    s.f();
+    return 0;
+}
