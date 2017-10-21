d8ccd5bf4091ce7c5b28cde70ea29020cd5ca8bd
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-21 11:13:55 +0200

    Initial Commit

---
+++ cpp17/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp17
+//
+//  Created by Jakub Łabeński on 21.10.2017.
+//  Copyright © 2017 Jakub Łabeński. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
