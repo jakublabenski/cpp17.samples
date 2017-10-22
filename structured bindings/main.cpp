40f7c1ec257247a3598ebc34cdda378fcf450b71
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-22 20:48:05 +0200

    structured bindings - example added.

---
+++ structured bindings/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  structured bindings
+//
+//  Created by Jakub Łabeński on 22.10.2017.
+//  Copyright © 2017 Jakub Łabeński. All rights reserved.
+//
+
+#include <iostream>
+//#include <pair>
+#include <tuple>
+#include <array>
+
+int array[5];
+
+// reference to elements:
+auto& [a1, a2, a3, a4, a5] = array;
+// copy of elements:
+auto [a6, a7, a8, a9, a10] = array;
+
+// for tuple:
+auto [i1, f1, c1] = std::make_tuple(1, 1.0, 'a');
+
+auto t = std::make_tuple(1, 2, "str");
+auto [i2, i3, str] = t;
+
+// for pair:
+auto [p1, p2] = std::make_pair('a', 1);
+
+// for array:
+std::array<bool, 3> ba;
+auto [b1, b2, b3] = ba;
+
+// for struct:
+struct S {
+    int i;
+    int bf:4;
+    float f;
+};
+
+auto [i, bf, f2] = S{};
+
+// number of exements must match:
+// auto [b5, b6] = ba;
+// auto or auto&, with possoble cv qualifiers required:
+// bool [b7, b8, b9] = ba;
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
