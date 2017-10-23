33507d240c5509349ac9534e84a1359445135edc
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-23 14:58:11 +0200

    removing deprecated exception specifications - examples added.

--- remove deprecated/main.cpp
+++ remove deprecated/main.cpp
@@ -6,7 +6,32 @@
 //  Copyright © 2017 Jakub Łabeński. All rights reserved.
 //
 
+#include <iostream>
+
+// removing deprecated exception specifications
+// Only three allowed excdption specification syntaxes:
+void f_noexcept() noexcept;
+void f() noexcept(true);
+// using throw() is deprecated:
+void f_throws() throw();
+
+#if __cplusplus < 201703L
+void f_int() throw(int, float); // ill-formed in C++17
+
+void on_terminate()
+{
+    // unexpected_handler, get_unexpected, set_unexpected, unexpected are
+    // removed from standard
+    std::unexpected_handler t = std::get_unexpected();
+    std::set_unexpected(t);
+    
+    std::unexpected();
+}
+
+#endif
+
 int main(int argc, const char * argv[]) {
+#if __cplusplus < 201703L
     bool b = false;
     
     // deprecated bool increment removed:
@@ -19,4 +44,5 @@ int main(int argc, const char * argv[]) {
     int a??(10??);
     
     return r;
+#endif
 }
