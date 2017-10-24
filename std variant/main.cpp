cb3ba39bc1944c8dc9ff99279939ce02bee22083
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-24 16:46:27 +0200

    std :: variant example added.

---
+++ std variant/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  std variant
+//
+//  Created by Jakub Łabeński on 24.10.2017.
+//  Copyright © 2017 Jakub Łabeński. All rights reserved.
+//
+
+#include <iostream>
+#include <variant>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
