6faa819f706eb5ea5c58ed34f530c35fcc19bdf4
Jakub Łabeński <jakub.labenski@gmail.com>
2017-10-24 21:55:04 +0200

    template argument deduction for class templates updated

--- template argument deduction for class templates/main.cpp
+++ template argument deduction for class templates/main.cpp
@@ -7,13 +7,24 @@
 //
 
 #include <iostream>
+#include <string>
 
 template <typename T>
 struct S {
     S(T t1, T t2) {}
 };
 
+// explicit deduction guides:
+template<typename T, T a = 0>
+S(T, T) -> S<double>;
+
+
 int main(int argc, const char * argv[]) {
-    auto s = S{1, 2};
+    auto s1 = S{1, 2};
+    S s2{'1', '2'};
+
+    std::cout << typeid(s1).name() << '\n';
+    std::cout << typeid(s2).name() << '\n';
+
     return 0;
 }
